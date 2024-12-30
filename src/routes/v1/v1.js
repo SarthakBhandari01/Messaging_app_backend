79a5a9fc611d5b92e0f93481afbb7e37a9010415
Sarthak <sarthakbhandari49@gmail.com>
2024-12-30 07:47:09 +0530

    Added code to fetch channel

--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -1,11 +1,13 @@
 import express from "express";
 
+import channelRouter from "./channel.js";
 import userRouter from "./user.js";
 import workspaceRouter from "./workspaceRoute.js";
 
 const router = express.Router();
 
 router.use("/user", userRouter);
-router.use("/workspace",workspaceRouter);
+router.use("/workspace", workspaceRouter);
+router.use("/channels", channelRouter);
 
 export default router;
---
+++ src/services/channelService.js
@@ -0,0 +1,32 @@
+import { StatusCodes } from "http-status-codes";
+
+import channelRepository from "../repositories/channelRepository.js";
+import ClientError from "../utils/errors/clientError.js";
+import { isUserMemberOfWorkspace } from "./workspaceService.js";
+
+export const getChannelByIdService = async (channelId, userId) => {
+  try {
+    const channel = await channelRepository.getChannelWithWorkspaceDetails(
+      channelId
+    );
+    if (!channel || !channel.workspaceId) {
+      throw new ClientError({
+        message: "Channel not found",
+        explaination: "Invalid data sent from the client ",
+        statusCode: StatusCodes.NOT_FOUND,
+      });
+    }
+    const isMember = isUserMemberOfWorkspace(channel.workspaceId, userId);
+    if (!isMember) {
+      throw new ClientError({
+        message: "User is not part of workspace hence cannot access channel ",
+        explaination: "User is not member of workspace",
+        statusCode: StatusCodes.FORBIDDEN,
+      });
+    }
+    return channel;
+  } catch (error) {
+    console.log("Get channel by id Service error ", error);
+    throw error;
+  }
+};
