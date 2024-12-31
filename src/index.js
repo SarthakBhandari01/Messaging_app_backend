a9cbc1e0beef5e42a5cdbc2a7ede9d522b43b759
Sarthak <sarthakbhandari49@gmail.com>
2024-12-31 07:57:42 +0530

    added is member part of workspace

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { StatusCodes } from "http-status-codes";
 
 import connectDB from "./config/dbConfig.js";
-import transporter from "./config/mailConfig.js";
+// import transporter from "./config/mailConfig.js";
 import { PORT } from "./config/serverConfig.js";
 import apiRouter from "./routes/apiRouter.js";
 
@@ -22,11 +22,11 @@ app.use("/api", apiRouter); //if the url starts with "/api" then the request is 
 app.listen(PORT, async () => {
   console.log("server running on port ", PORT);
   connectDB();
-  const mailResponse =await transporter.sendMail({
-    from: "sarthakbhandri49@gmail.com",
-    to: "sarthakbhandri49@gmail.com",
-    subject: "Welcome mail",
-    text: "Welcome to the app",
-  });
-  console.log(mailResponse);
+  // const mailResponse =await transporter.sendMail({
+  //   from: "sarthakbhandri49@gmail.com",
+  //   to: "sarthakbhandri49@gmail.com",
+  //   subject: "Welcome mail",
+  //   text: "Welcome to the app",
+  // });
+  // console.log(mailResponse);
 });
--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -1,13 +1,15 @@
 import express from "express";
 
 import channelRouter from "./channel.js";
+import memberRouter from "./member.js";
 import userRouter from "./user.js";
 import workspaceRouter from "./workspaceRoute.js";
 
 const router = express.Router();
 
-router.use("/user", userRouter);
-router.use("/workspace", workspaceRouter);
+router.use("/users", userRouter);
+router.use("/workspaces", workspaceRouter);
 router.use("/channels", channelRouter);
+router.use("/members", memberRouter);
 
 export default router;
