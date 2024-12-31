78a1b7cdf84df1c50e56f5478f2fa75c017a03e3
Sarthak <sarthakbhandari49@gmail.com>
2024-12-31 14:19:02 +0530

    Added Get all messages

--- src/routes/v1/v1.js
+++ src/routes/v1/v1.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import channelRouter from "./channel.js";
 import memberRouter from "./member.js";
+import messageRouter from "./message.js"
 import userRouter from "./user.js";
 import workspaceRouter from "./workspaceRoute.js";
 
@@ -11,5 +12,6 @@ router.use("/users", userRouter);
 router.use("/workspaces", workspaceRouter);
 router.use("/channels", channelRouter);
 router.use("/members", memberRouter);
+router.use("/messages",messageRouter);
 
 export default router;
